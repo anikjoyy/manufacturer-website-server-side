e4dd0fd23bc2705cfdec51e525e94807062a7baf
anikjoyy <anikjoy.cse@gmail.com>
2022-05-25 15:36:15 +0600

    build database for store all the product

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,28 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gr2td.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const bookCollection = client.db('carWhisperer').collection('product');
+
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = bookCollection.find(query);
+      const books = await cursor.toArray();
+      res.send(books);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('The car whisperer is whispering successfully');
